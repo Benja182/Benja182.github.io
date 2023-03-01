38cd7dea2f4ca6a5934844c39ef847d278780db0
Benja182 <126586395+Benja182@users.noreply.github.com>
2023-02-28 23:03:32 -0300

    Primer Proyecto

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+
+//*function consoleText(words, id, colors) {
+  //if (colors === undefined) colors = ['#fff'];
+  //var visible = true;
+  //var con = document.getElementById('console');
+  //var letterCount = 1;
+  //var x = 1;
+  //var waiting = false;
+  //var target = document.getElementById(id)
+  //*/target.setAttribute('style', 'color:' + colors[0])
